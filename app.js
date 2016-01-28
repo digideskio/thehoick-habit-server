335c616fab6c4d200ac1e062994a840446d08fec
Adam Sommer <adam@thehoick.com>
2016-01-28 11:50:35 -0500

    Importing Help file, and have JSON REST API working for getting, creating, updating, and deleting users.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var bodyParser = require('body-parser')
+var favicon = require('serve-favicon');
+
+var PouchDB = require('pouchdb');
+var db = new PouchDB('habits');
+var remoteCouch = false;
+
 var app = express();
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // GET /assets (static files).
 app.use('/assets', express.static(__dirname + '/public'));
@@ -9,32 +19,90 @@ app.use('/assets', express.static(__dirname + '/public'));
 // Return a simple welcome message.
 app.get('/', function(req, res) {
   fs.readFile('./public/welcome.html', function (err, html) {
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
+    // res.writeHeader(200, {"Content-Type": "text/html"});
+    // res.write(html);
+    // res.end();
+    res.set('Content-Type', 'text/html');
+    res.send(html);
   });
 });
 
-// GET /habits/$username
+// GET /habits/:username
 app.get('/habits/:username', function(req, res) {
-  res.send('<html><head></head><body><h1>Username: ' + req.params.username + '</body></html>');
-  //    Get habits array from database.
-  //    Convert it to JSON.
-  //    Serve it back as JSON.
+  // Get habits array from database.
+  db.get(req.params.username, function (error, doc) {
+    if (error) {
+      res.status(404).json(error);
+    }
+    res.json(doc);
+  });
 });
 
-  // POST /habits/$username
-  //     Recieve JSON data.
-  //     Save JSON data to database.
-  //     Send back confirmation in JSON.
+// POST /habits/:username
+app.post('/habits', function(req, res) {
+  // Look for the document in the database.
+  db.get(req.body.username, (error, doc) => {
+    // If there is no document for that user create one.
+    if (error && error.status == 404) {
+      // Use username for _id.
+      req.body._id = req.body.username;
+
+      // Create new doc.
+      db.put(req.body, (error, newDoc) => {
+        if (error) {
+          res.status(error.status).json(error);
+        }
+
+        res.json(newDoc);
+      });
+    } else {
+      // Update the doc.
+      db.put(req.body, doc._id, doc._rev, (error, updatedDoc) => {
+        if (error) {
+          res.status(error.status).json(error);
+        }
+
+        res.json(updatedDoc);
+      });
+    }
+  })
+});
+
+// DELETE /habits/:username
+app.delete('/habits', function(req, res) {
+  // Lookup username.
+  db.get(req.body.username, function (error, doc) {
+    if (error) {
+      res.status(error.status).json(error);
+    }
+
+    // Remove the doc and send status message.
+    db.remove(doc);
+    res.json({username: req.params.username, message: 'Removed from database.'});
+  });
+});
+
+// GET /help
+// Return a simple 404 page.
+app.get('/help', function(req, res) {
+  fs.readFile('./public/help.html', function (err, html) {
+    // res.writeHeader(200, {"Content-Type": "text/html"});
+    // res.write(html);
+    // res.end();
+    res.set('Content-Type', 'text/html');
+    res.send(html);
+  });
+});
 
 // GET 404
 // Return a simple 404 page.
 app.get(/.*/, function(req, res) {
   fs.readFile('./public/404.html', function (err, html) {
-    res.writeHeader(200, {"Content-Type": "text/html"});
-    res.write(html);
-    res.end();
+    // res.writeHeader(200, {"Content-Type": "text/html"});
+    // res.write(html);
+    // res.end();
+    res.set('Content-Type', 'text/html');
+    res.send(html);
   });
 });
 
